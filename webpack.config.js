c3809ab6dfa521a8edf5c40e14895720a776f6f6
Chris Cortese <chris@chriscortese.net>
2019-04-26 17:22:03 -0700

    fix bundle of all js chunks

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const glob = require("glob")
 
 module.exports = {
   entry: {
-    "bundle.js": glob.sync("build/static/?(js|css)/main.*.?(js|css)").map(f => path.resolve(__dirname, f)),
+    "bundle.js": glob.sync("build/static/?(js|css)/*.?(js|css)").map(f => path.resolve(__dirname, f)),
   },
   output: {
     filename: "build/static/js/bundle.min.js",
