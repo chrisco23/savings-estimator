0bc4f28d9fed5ef1b5f96d2f80aff2f2fae33713
Rob Fenwick <robfenwick@msn.com>
2019-05-26 19:58:09 -0700

    Update IndividualResults.js
    add credit to ProgCode

--- src/components/IndividualResults.js
+++ src/components/IndividualResults.js
@@ -161,6 +161,10 @@ const IndividualResults = ({
       <div className="wwse-startover">
         <button onClick={refreshPage}>Start Over</button>
       </div>
+      <div className="wwse-credit">
+        <p>This tool would not be possible without the amazing talent and effort of the team at <a href="https://www.progcode.org/" 
+          target="_blank" rel="noopener noreferrer">Progressive Coders Network</a>.</p>
+      </div>
     </div>
   )
 }
